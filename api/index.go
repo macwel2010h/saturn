5d17166bffa232b2ad585290a76fd16068baf3f9
macwel2010h <macw.el2010@hotmail.com>
2026-05-26 21:38:19 +0530

    ui changes

--- api/index.go
+++ api/index.go
@@ -6,7 +6,7 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("web/html/index.html", "web/html/t_navbar.html", "web/html/t_logo.html")
+	ts, err := template.ParseFiles("web/html/index.html", "web/html/t_navbar.html", "web/html/t_logo.html", "web/html/t_footer.html")
 	if err != nil {
 		ServerError(w, r, err)
 		return
--- api/welcome.go
+++ api/welcome.go
@@ -6,7 +6,7 @@ import (
 )
 
 func WelcomeHandler(w http.ResponseWriter, r *http.Request) {
-	ts, err := template.ParseFiles("web/html/welcome.html", "web/html/t_navbar.html", "web/html/t_logo.html")
+	ts, err := template.ParseFiles("web/html/welcome.html", "web/html/t_navbar.html", "web/html/t_logo.html", "web/html/t_footer.html")
 
 	if err != nil {
 		ServerError(w, r, err)
